df0688a9aba49fef5f99a3affe06ccfc62132173
miyeaier <miyesier@gmail.com>
2022-01-22 16:47:29 +0100

    Create a new request spec api_responds_with_a_specific_article.rb and test all go green

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,4 +3,10 @@ class Api::ArticlesController < ApplicationController
     articles = Article.all
     render json: { articles: articles }
   end
+
+  def show
+    article = Article.find(params['id'])
+    render json: { article: article }
+
+  end
 end
