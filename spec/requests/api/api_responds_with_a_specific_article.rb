a94f807625fc1aceb0115f461123995bc0495fe0
miyeaier <miyesier@gmail.com>
2022-03-03 17:18:24 +0100

    add DS_STORE snd modified spec

--- spec/requests/api/api_responds_with_a_specific_article.rb
+++ spec/requests/api/api_responds_with_a_specific_article.rb
@@ -1,21 +1,35 @@
-RSpec.describe 'GET /api/articles/:id' do
-  describe 'successfully' do
-    let!(:article) {create(:article, title: 'This is the latest news', body: 'And this is some mind blowing content')}
+RSpec.describe "GET /api/articles/:id" do
+  describe "successfully" do
+    let!(:article) { create(:article, title: "This is the latest news", body: "And this is some mind blowing content") }
 
     before do
       get "/api/articles/#{article.id}"
     end
 
-    it 'is exptected to return a 200 status' do
+    it "is exptected to return a 200 status" do
       expect(response).to have_http_status 200
     end
 
-    it 'is expected to return the requested articles title' do
-      expect(response_json['article']['title']).to eq 'This is the latest news'
+    it "is expected to return the requested articles title" do
+      expect(response_json["article"]["title"]).to eq "This is the latest news"
     end
-    
-    it 'is expected to return the requested articles body' do
-      expect(response_json['article']['body']).to eq 'And this is some mind blowing content'
+
+    it "is expected to return the requested articles body" do
+      expect(response_json["article"]["body"]).to eq "And this is some mind blowing content"
     end
-  end 
+  end
+
+  describe "unsuccessfully with invalid id" do
+    before do
+      get "api/articles/abc"
+    end
+
+    it "is expected to return a 404 status" do
+      expect(response).to have_http_status 404
+    end
+
+    it "is expected to return an error message" do
+      expect(response_json["message"]).to eq "Unfortunately we can not find the article you are looking for."
+    end
+  end
 end
